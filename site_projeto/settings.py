aa21f32dd64bbc2d1da1afbae22c93b9c7640435
Gvzadaa <gabrielramos1278@gmail.com>
2025-05-30 20:27:07 -0300

    adicionando aspas duplas no settings

--- site_projeto/settings.py
+++ site_projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=0cfx9-a6josdl(+7&xy9133wv$6baqfi5^#v(xvbws7_b%c!h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
